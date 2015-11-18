a19b3175ad38eb166925b7afda4c667879aa6669
xiaoyu12345 <20141105060w@Mac12.local>
2015-11-18 19:51:43 +0800

    指针数组
    把先前的一个变量改为数组，因为定义的数组为int型，所以输出的位置之间间隔为4，double为8，char为1。指针就是位置。

--- main.cpp
+++ main.cpp
@@ -10,6 +10,14 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    int a[4];
+    int *p;
+    a[0]=5;
+    a[1]=10;
+    a[2]=15;
+    a[3]=20;
+    p=a;
+    printf("&a=%x     &a[0]=%x    &a[1]=%x     &a[2]=%x    &a[3]=%x\n  ",a,&a[0],&a[1],&a[2],&a[3]);
+    printf("a[0]=%d\n",*(a+1));
     return 0;
 }
