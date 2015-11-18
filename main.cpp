88bf208177905e62db8324226610f9efc7bfb7da
xiaoyu12345 <20141105060w@Mac12.local>
2015-11-18 19:39:01 +0800

    第一次 提交

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  pointer
+//
+//  Created by 小鱼🐟 on 15/11/18.
+//  Copyright (c) 2015年 小鱼🐟. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
